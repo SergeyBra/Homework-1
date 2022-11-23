234c47345c814678ddb8e3e1eadcff49bc94b63a
Sergey B <brazhnikov_serge@mail.ru>
2022-11-24 00:54:48 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numC= int.Parse(Console.ReadLine());
+if (numA>numB) 
+{
+    if (numA>numC)
+    {
+     Console.Write($"max = {numA}");
+    }
+    else 
+    {
+    Console.Write($"max = {numC}");
+    }
+}
+else
+{
+    if (numB>numC)
+    {
+     Console.Write($"max = {numB}");
+    }
+    else 
+    {
+    Console.Write($"max = {numC}");
+    }
+}
