62f2c6fc7f7ab2883d95250d037cc0e48363a909
Sergey B <brazhnikov_serge@mail.ru>
2022-11-24 01:04:38 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите число");
+int numA = int.Parse(Console.ReadLine());
+if (numA%2==0)
+{
+Console.Write($"Число {numA} является четным");
+}
+else
+{
+Console.Write($"Число {numA} является нечетным");    
+}
