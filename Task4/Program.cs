6b9681c097de7b8ce41abcbb1d0efb1dddf72aaa
Sergey B <brazhnikov_serge@mail.ru>
2022-11-24 01:22:42 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write($"Все четные числа от 1 до {N}: ");
+do
+{
+    if (i%2==0)
+    {
+    Console.Write($"{i}, ");
+    }
+    i++;
+} while (i <= N);
+
+
